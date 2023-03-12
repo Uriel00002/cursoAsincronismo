3be7f69e71ed4447cb214ebcae2f9e350f2db9f5
leiru <uriellopezaquino1.0@gmail.com>
2023-03-11 21:19:07 -0600

    XMLHTTPRequest

--- src/callbacks/challenge.js
+++ src/callbacks/challenge.js
@@ -1,12 +1,12 @@
 const XMLHttpRequest = require('xmlhttprequest');
-const API = 'https://api.escuelsjs.co/api/v1'
+const API = 'https://api.escuelajs.co/api/v1'
 
 function fetchData(urlApi, callback){
    let xhttp = new XMLHttpRequest();
 
    xhttp.open('GET',urlApi, true);
    xhttp.onreadystatechange = function (event){
-       if(xhttp.onreadystatechange === 4){
+       if(xhttp.readyState === 4){
            if(xhttp.status === 200){
                callback(null, JSON.parse(xhttp.responseText));
            }
